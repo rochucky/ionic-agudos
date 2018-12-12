52a5dbe2d049de2734891f1005544e02ebf587ea
Rodrigo Alves <rodrigo.alves@bexpert.lan.corp>
2018-12-12 18:57:15 -0200

    Transições de tabs para o estabeleciimento

--- src/pages/tabs-e/tabs-e.ts
+++ src/pages/tabs-e/tabs-e.ts
@@ -1,18 +1,24 @@
 import { Component, ViewChild } from '@angular/core';
-import { Slides, Tabs } from 'ionic-angular';
+import { App, Slides, Tabs, AlertController, LoadingController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { VendaPage } from '../venda/venda';
 import { TransacoesEPage } from '../transacoes-e/transacoes-e';
+import { HomePage } from '../home/home';
 
 @Component({
+  selector: 'page-tabs',
   templateUrl: 'tabs-e.html'
 })
+
 export class TabsEPage {
 
   tab1Root = VendaPage;
   tab2Root = TransacoesEPage;
 
-  constructor() {
+  title: any = 'Venda';
+
+  constructor(public app: App, public alertCtrl: AlertController, public loadingCtrl: LoadingController, public storage: Storage,) {
 
   }
 
@@ -22,16 +28,14 @@ export class TabsEPage {
 	slideChange(){
   //console.log(this.slides.getActiveIndex());
    if(this.slides.getActiveIndex() == 0){
+      this.title = 'Venda';
       this.menuTabs.select(0);
       this.slides.slideTo(0);
     }
     if(this.slides.getActiveIndex() == 1){
+      this.title = 'Transações';
       this.menuTabs.select(1);
       this.slides.slideTo(1);
-    }
-    if(this.slides.getActiveIndex() == 2){
-      this.menuTabs.select(2);
-      this.slides.slideTo(2);
     }
 	}
 
@@ -39,17 +43,48 @@ export class TabsEPage {
     //console.log("tabchange");
     //console.log(this.menuTabs.getSelected().index);
     if(this.menuTabs.getSelected().index == 0){
+      this.title = 'Venda';
       this.menuTabs.select(0);
       this.slides.slideTo(0);
     }
     if(this.menuTabs.getSelected().index == 1){
+      this.title = 'Transações';
       this.menuTabs.select(1);
       this.slides.slideTo(1);
     }
-    if(this.menuTabs.getSelected().index == 2){
-      this.menuTabs.select(2);
-      this.slides.slideTo(2);
-    }
-  }  
+  } 
+
+  logout(){
+    let confirm = this.alertCtrl.create({
+      title: 'Logout',
+      subTitle: 'Deseja realmente fazer logout?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: 'Carregando'
+            });
+
+            loading.present();
+
+            this.storage.clear().then(() => {
+              loading.dismiss();
+              this.app.getRootNav().setRoot(HomePage);
+              
+            });
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
 }
